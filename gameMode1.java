74015abb294e619d0e0faa8c85c21d7b213254a1
smcrider-png <smcrider@iu.edu>
2026-04-05 08:58:12 -0400

    randomLetter method in gameMode1
    Added a method to pull a random letter from the alphabet and return a file name.

--- gameMode1.java
+++ gameMode1.java
@@ -1,2 +1,17 @@
+import java.util.List;
+import java.util.stream.Stream;
+
 public class gameMode1 {
+
+    /**
+     * Pulls a random finger sign
+     * @return file name
+     */
+    String randomLetter() {
+        int n = (int) (Math.random() * 100);
+        List<Character> alphabet = Stream.of(
+                'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                'n','o','p','q','r','s','t','u','v','w','x','y','z').toList();
+        return "asl_" + alphabet.get(n) + ".jpg"; // TODO: check file type
+    }
 }
