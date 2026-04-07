08bbb529a605524e1a0d4f3864d93036b19a8b5b
smcrider-png <smcrider@iu.edu>
2026-04-07 08:50:00 -0400

    Implemented writeScore()
    Added a method to write the score to a file named with the current date and time. Added a variable called this.SCORE.

--- gameMode1.java
+++ gameMode1.java
@@ -1,7 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class gameMode1 {
+
+    private int SCORE = 0;
 
     /**
      * Pulls a random finger sign
@@ -13,5 +18,18 @@ public class gameMode1 {
                 'a','b','c','d','e','f','g','h','i','j','k','l','m',
                 'n','o','p','q','r','s','t','u','v','w','x','y','z').toList();
         return "asl-" + alphabet.get(n) + ".png";
+    }
+
+    /**
+     * Writes the final score to a file
+     */
+    void writeScore() {
+        String currTime = LocalDateTime.now().toString();
+        try (FileWriter fw = new FileWriter(currTime + ".out")) {
+            fw.write(currTime);
+            fw.write(" - Game Mode 1 Score: " + this.SCORE);    // TODO: rename the game mode
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);     // throw exception if error occurs
+        }
     }
 }
