8a448de5b40f10e06e7614a0fdf72443efc85189
smcrider-png <smcrider@iu.edu>
2026-04-27 17:44:41 -0400

    Removed unncessary "implements" call

--- gameMode1.java
+++ gameMode1.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class gameMode1  extends gameModes implements IGameInstance{
+public class gameMode1 extends gameModes {
 
     int SCORE;
     private char currLetter;
