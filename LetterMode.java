0af29d9cbc876e03302508b6e85167a0d7e67996
smcrider-png <smcrider@iu.edu>
2026-04-29 20:31:48 -0400

    Deleted GameModes class

--- LetterMode.java
+++ LetterMode.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public final class LetterMode extends GameModes {
+public final class LetterMode implements IGameInstance {
 
     int SCORE;
     private char currLetter;
